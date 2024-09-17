7051b43f959e030f70d3ac805c4befebc58ebb26
RohitPawar001 <rppawar491@gmail.com>
2024-09-17 18:53:09 +0530

    initial commit

--- app.py
+++ app.py
@@ -21,6 +21,13 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route("/predict",methods=["post"])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output = reg_model.predict(final_input)[0]
+    return render_template("home.html",prediction_text=f"The pridicated price is {output}")
 
 if __name__ == "__main__":
     app.run(debug=True)
